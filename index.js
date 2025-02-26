57d28ddf30d1e40cb6683a9dd0dc2eb1e4155f51
Abhinav-Rusia <abhinavrusia@gmail.com>
2025-02-26 09:07:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.listen(5000, () => console.log("server started at port http://localhost:5000"))
